82cb167f659f434420fbce9d9976e3fddb412413
BMWire <bmwemawire@gmail.com>
2021-06-17 15:56:42 +0300

    Hot fix 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Main entry point for the program
+ * @void: The function does not accept any parameters
+ * Return: int returned as 0
+ */
 int main(void)
 {
-    char quote = 'and that piece of art is useful" - Dora Korpar, 2015-10-19';
+char quote = 'and that piece of art is useful" - Dora Korpar, 2015-10-19';
 
-    putchar(quote);
-    return 1;
+putchar(quote);
+return (1);
 }
