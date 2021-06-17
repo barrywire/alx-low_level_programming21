226386911a1145dfb46a51af0e92e78dff74cea1
BMWire <bmwemawire@gmail.com>
2021-06-17 15:44:34 +0300

    Hot fix 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Main entry point for the program
+ * @void: The function does not accept any parameters
+ * Return: int returned as 0
+ */
 int main(void)
 {
-    puts('"Programming is like building a multilingual puzzle');
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
