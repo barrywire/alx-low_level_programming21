d54ba56005fe9aba5176b956a83cc5f2a85661a7
BMWire <bmwemawire@gmail.com>
2021-06-17 15:42:20 +0300

    Hot fix 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ sizeLongLongInt = sizeof(long long int);
 sizeFloat = sizeof(float);
 
 printf("Size of a char: %d byte(s)\n", sizeChar);
-printf("Size of a int: %d byte(s)\n", sizeInt);
+printf("Size of an int: %d byte(s)\n", sizeInt);
 printf("Size of a long int: %d byte(s)\n", sizeLongInt);
 printf("Size of a long long int: %d byte(s)\n", sizeLongLongInt);
 printf("Size of a float: %d byte(s)\n", sizeFloat);
