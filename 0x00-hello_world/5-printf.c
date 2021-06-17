78d3ae7a35fd631c7a72dce78e36883dcc5fe07e
BMWire <bmwemawire@gmail.com>
2021-06-17 15:33:16 +0300

    Hot fix 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Main entry point for the program 
+ * @void: The function does not accept any parameters
+ * 
+ * Return: int returned as 0 
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art, \n");
